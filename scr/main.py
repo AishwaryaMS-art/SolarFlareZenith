8f496f06f547543ddb5f33fe60621b00a8b7236b
AishwaryaMS-art <msaishwarya35@jnn.edu.in>
2025-11-23 19:25:49 +0530

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -1,1 +1,11 @@
+import google.generativeai as genai
+
+genai.configure(api_key="YOUR_API_KEY")
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+while True:
+    user = input("You: ")
+    response = model.generate_content(user)
+    print("AI:", response.text)
 
